37ba6d757cd29f23c7d12f07927cf7a36ff3f889
Rajat Rajdeep <rajatrajdeep@outlook.com>
2024-10-28 01:07:16 +0530

    Fix BootstrapJs component export

--- src/components/BootstrapJs.jsx
+++ src/components/BootstrapJs.jsx
@@ -2,10 +2,12 @@
 
 import { useEffect } from 'react';
 
-export default function BootstrapJs() {
+const BootstrapJs = () => {
   useEffect(() => {
     import('bootstrap/dist/js/bootstrap.min.js');
   }, []);
 
   return null;
-}
+};
+
+export default BootstrapJs;
