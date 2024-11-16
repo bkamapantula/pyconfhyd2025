b97334187a649b0ca316c81d623c4ddbb0a2a9f4
Rajat Rajdeep <rajatrajdeep@outlook.com>
2024-11-16 12:57:00 +0530

    Add COC

--- data/details.js
+++ data/details.js
@@ -14,6 +14,12 @@ export const NAV_ITEMS = [
   {
     name: 'Home',
     path: '/',
+    target: '_self',
+  },
+  {
+    name: 'Code Of Conduct',
+    path: 'https://docs.google.com/document/d/1yjPaVdsQCbowelwI1LlGco3CFGG3vZKhGiMYXDOOKrY',
+    target: '_blank',
   },
 ];
 
