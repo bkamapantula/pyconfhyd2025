48eed3b4c43d971d4264f009d37f740b75c9077f
Rajat Rajdeep <rajatrajdeep@outlook.com>
2025-01-07 15:22:16 +0530

    Add static export in nextconfig

--- next.config.mjs
+++ next.config.mjs
@@ -2,11 +2,14 @@ import createMDX from '@next/mdx';
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
   // Configure `pageExtensions` to include markdown and MDX files
   pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
 };
 
-const withMDX = createMDX({});
+const withMDX = createMDX({
+  extension: /\.mdx?$/, // Include .mdx files
+});
 
 // Merge MDX config with Next.js config
 export default withMDX(nextConfig);
